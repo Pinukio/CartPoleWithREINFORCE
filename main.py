88a22e73d77533c02f234ba6c65e806f7e8b4968
Pinukio <gkswjddnjs15@gmail.com>
2024-01-17 23:19:23 +0900

    add main function

--- main.py
+++ main.py
@@ -40,3 +40,31 @@ class Policy(nn.Module):
             loss.backward()
         self.optimizer.step()
         self.data = []
+
+def main():
+    env = gym.make('CartPole-v1')
+    pi = Policy()
+    score = 0.0
+    print_interval = 20
+
+    for n_epi in range(10000):
+        s = env.reset()[0]
+        done = False
+
+        while not done:
+            prob = pi.forward(torch.from_numpy(s).float())
+            # Categorical.sample()은 확률 분포에 의한 Sampling을 진행해 준다.
+            # Episode 진행 과정에서 Policy에 의한 Action 선택임
+            m = Categorical(prob)
+            action = m.sample()
+            s_prime, reward, done, trun, _ = env.step(action.item())
+            # Episode가 진행되는 동안 Data를 수집함
+            pi.put_data((reward, prob[action]))
+            s = s_prime
+            score += reward
+
+        pi.train_net()
+        if n_epi % print_interval == 0 and n_epi != 0:
+            print("# of episode: {}, avg score: {}".format(n_epi, score/print_interval))
+            score = 0.0
+        env.close()
