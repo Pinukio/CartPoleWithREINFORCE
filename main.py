5b84507c3392eff982471608e3e26978a6a9c21f
Pinukio <gkswjddnjs15@gmail.com>
2024-01-17 23:00:03 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+# 바닥부터 배우는 강화 학습 P.228 REINFORCE 구현
+
+import gym
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Categorical
+
+# Hyperparameters
+learning_rate = 0.0002
+gamma = 0.98
+
+class Policy(nn.Module):
+    def __init__(self):
+        super(Policy, self).__init__()
+        self.data = []
+
+        self.fc1 = nn.Linear(4, 128)
+        self.fc2 = nn.Linear(128, 2)
+        self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.softmax(self.fc2(x), dim=0)
+        return x
+    
+    def put_data(self, item):
+        self.data.append(item)
+
+    def train_net(self):
+        R = 0
+        self.optimizer.zero_grad()
+        for r, prob in self.data[::-1]:
+            # Return
+            R = r + gamma * R
+            # prob: Policy에 의한 Action 선택 확률
+            loss = -R * torch.log(prob)
+            # Gradient 계산
+            loss.backward()
+        self.optimizer.step()
+        self.data = []
